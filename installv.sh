02ae3690cd29b5342c904be67ed609a382d4d145
x7hh <jozefdevil2@gmail.com>
2017-03-20 00:34:07 +0300

    Add files via upload

---
+++ installv.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+cd new_x7bot
+‏chmod +x x7bot.sh
+‏./x7bot.sh install
+‏./x7bot.sh
+‏./x7bot.sh install
+‏./x7bot.sh
+
+
+
+echo -e "${CYAN}Installation Completed! Create a bot with creator.lua (lua creator.lua)${NC}"
+
+exit
+--install.sh with lua by @abod_evo
