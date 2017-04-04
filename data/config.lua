2e4ff4f0ce7c13b5d12cbe386eacea68947afb5f
x7hh <jozefdevil2@gmail.com>
2017-04-04 04:06:23 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,27 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "subergroup",
+    "checks",
+    "plugins",
+    "super2",
+    "me",
+    "re_replay",
+    "Help",
+    "lock_tafia",
+    "lock_badword",
+    "lock_smiles",
+    "lock_rdod",
+  },
+  info_text = "",
+  moderation = {
+    data = "./data/moderation.json",
+  },
+  sudo_users = {
+     128549161,
+  }
+}
+return _
+end
