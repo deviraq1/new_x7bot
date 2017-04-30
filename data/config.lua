15b4ea1b34c88c779982047cafd2be54a8686a77
deviraq1 <jokrbot2017@gmail.com>
2017-04-30 20:00:56 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,40 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "addkick",
+    "subergroup",
+    "help6",
+    "del",
+    "checks",
+    "pohot",
+    "plugins",
+    "super2",
+    "me",
+    "jokr",
+    "re_replay",
+    "help",
+    "help1",
+    "help2",
+    "help3",
+    "help4",
+    "help5",
+    "pic",
+    "text",
+    "lock_tafia",
+    "lock_badword",
+    "lock_smiles",
+    "lock_rdod",
+  },
+  info_text = "🏌 ∂єνєℓσρєяѕ тєαм ☂\n\n🔹 ĐёⓋ 🔸@dev_iraq1\n🔹 ĐёⓋ 🔸@x7_newbot\n🔸 Channle 🔹@dev_iraq2",
+",
+  moderation = {
+    data = "./data/moderation.json",
+  },
+  sudo_users = {
+     209099061,
+  }
+}
+return _
+end
